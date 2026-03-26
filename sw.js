26f61ccbbdf70f847e4804cc304019ededaa17c2
salonherrera50-collab <salonherrera50@gmail.com>
2026-03-26 17:38:39 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // --- 1. NOMBRE DE LA VERSIÓN ---
 // ¡IMPORTANTE! Cambia 'v1.1' a 'v1.2' la próxima vez que subas cambios
-const CACHE_NAME = 'v7.0'; 
+const CACHE_NAME = 'v8.0'; 
 
 const assets = [
   '/',
